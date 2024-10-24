e4d11a72b9e020171ca7b71de89e9dc70c3c6e43
sans <simonesansalone777@gmail.com>
2024-10-24 04:38:40 +0200

    update 'agent with tools' chain type

--- chains/chain_scripts/agent_with_tools.py
+++ chains/chain_scripts/agent_with_tools.py
@@ -5,7 +5,7 @@ from langchain import hub
 from langchain.agents import AgentExecutor, create_openai_tools_agent
 from langchain.tools import tool
 from langchain_core.callbacks import Callbacks
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate
 from langchain_openai import ChatOpenAI
 
 from chains.chain_scripts.utilities.dataloader import DocumentToolKitManager
@@ -38,7 +38,10 @@ def get_chain(llm: Any = None,
 
     # Get the prompt to use - you can modify this!
     prompt = hub.pull("hwchase17/openai-tools-agent")
-    # print(prompt.messages) -- to see the prompt
+    #print(prompt.messages) #-- to see the prompt
+    prompt.messages[0] = ("system", """
+    write prompt here...
+    """)
 
     agent = create_openai_tools_agent(
         llm.with_config({"tags": ["agent_llm"]}), tools, prompt
@@ -51,50 +54,3 @@ def get_chain(llm: Any = None,
 #import pprint
 
 #chunks = []
-
-"""
-async def main():
-
-    chain = get_chain(llm=model)
-
-    async for event in chain.astream_events(
-            {"input": "create a cusotmer info tables about www.goldsolarweb.com"},
-            version="v1",
-    ):
-        kind = event["event"]
-        if kind == "on_chain_start":
-            if (
-                    event["name"] == "Agent"
-            ):  # Was assigned when creating the agent with `.with_config({"run_name": "Agent"})`
-                print(
-                    f"Starting agent: {event['name']} with input: {event['data'].get('input')}"
-                )
-        elif kind == "on_chain_end":
-            if (
-                    event["name"] == "Agent"
-            ):  # Was assigned when creating the agent with `.with_config({"run_name": "Agent"})`
-                print()
-                print("--")
-                print(
-                    f"Done agent: {event['name']} with output: {event['data'].get('output')['output']}"
-                )
-        if kind == "on_chat_model_stream":
-            content = event["data"]["chunk"].content
-            if content:
-                # Empty content in the context of OpenAI means
-                # that the model is asking for a tool to be invoked.
-                # So we only print non-empty content
-                print(content, end="|")
-                yield content
-        elif kind == "on_tool_start":
-            print("--")
-            print(
-                f"Starting tool: {event['name']} with inputs: {event['data'].get('input')}"
-            )
-        elif kind == "on_tool_end":
-            print(f"Done tool: {event['name']}")
-            print(f"Tool output was: {event['data'].get('output')}")
-            print("--")
-
-#asyncio.run(main())"""
-
